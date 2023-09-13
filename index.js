baa887e16136866c4e408d342c9bd7d21b887f58
Larsjep <lj@linel.dk>
2023-09-13 09:17:15 +0200

    More WIP

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import Chart from 'chart.js/auto';
 import data_points from './data.json';
-import {opgave1} from './opgave1.js'
+import { opgave1 } from './opgave1.js';
 
 opgave1();
+
+// document.getElementById('Recalculate').onclick = opgave1
+
+export function recalc() {
+  console.info('Foo');
+}
--- opgave1.js
+++ opgave1.js
@@ -2,18 +2,45 @@ import Chart from 'chart.js/auto';
 import data_points from './data.json';
 
 export function opgave1() {
-  const data = data_points;
+  var data = data_points;
 
-  new Chart(document.getElementById('plot1'), {
-    type: 'line',
+  var chart = new Chart(document.getElementById('plot1'), {
+    type: 'scatter',
     data: {
-      labels: data.map((row) => row.year),
+      //labels: data.map((row) => row.year),
+      backgroundColor: 'rgb(255, 99, 123)',
       datasets: [
         {
           label: 'Measured',
-          data: data.map((row) => row.count),
+          data: data.map((row) => {
+            return { x: row.ref, y: row.measured };
+          }),
         },
       ],
     },
+    options: {
+      scales: {
+        x: {
+          type: 'linear',
+          position: 'bottom',
+          beginAtZero: true,
+        },
+        y: {
+          type: 'linear',
+          position: 'left',
+          max: 13,
+          beginAtZero: true,
+        },
+      },
+    },
   });
+
+  document.getElementById('Recalculate').onclick = function () {
+    var slope = document.getElementById('Slope').value;
+    console.info(document.getElementById('Slope'));
+    chart.data.datasets[0].data = data.map((row) => {
+      return { x: row.ref, y: row.measured * slope };
+    });
+    chart.update();
+  };
 }
